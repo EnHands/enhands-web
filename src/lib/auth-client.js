f752dcf83be42e9814cbde2029e8755b1a6f55be
Sandario <konsti.eisebitt@gmail.com>
2026-05-26 16:58:18 +0200

    Quick Fix for Better Auth to use correct Path

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,5 +3,7 @@ import { createAuthClient } from "better-auth/svelte";
 
 export const authClient = createAuthClient({
     // This tells the client where your server is running
-    baseURL: "http://localhost:5173" 
+    // baseURL: "http://localhost:5173" 
+    // By omitting baseURL entirely (or leaving it blank), Better-Auth safely 
+    // uses relative paths (e.g., /api/auth/*) based on the active browser tab!
 });
